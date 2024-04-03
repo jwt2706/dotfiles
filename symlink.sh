aeff067e92cbec8cf6d2afb072a15349cafec120
jwt2706 <jwt2home@gmail.com>
2024-04-02 21:02:21 -0400

    symlink

--- symlink.sh
+++ symlink.sh
@@ -1,2 +1,9 @@
-ls -s ~/dotfiles/.bashrc ~/.bashrc
-ls -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+#!/bin/bash
+
+# symlinks
+ln -sf $(pwd)/.bashrc ~/.bashrc
+ln -sf $(pwd)/.bash_aliases ~/.bash_aliases
+
+# install stuff
+sudo apt update
+sudo apt install -y vim git
